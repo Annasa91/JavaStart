1b9d9914003826b513e5ca207873bed83c5b9cf6
Annasa91 <annasa91@mail.ru>
2020-05-29 17:16:41 +0500

    Задание ко второму уроку

---
+++ Lesson2.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class Lesson2 {
+    public static void Array1() {
+        int[] array = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for (int i = 0; i < array.length; i++) {
+            if ( array[i] == 1) {
+                array[i] == 0
+            } else {
+                array[i] == 1
+            }
+        }
+    }
+}
